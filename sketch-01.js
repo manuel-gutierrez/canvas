50d8d511641c9df9f2eb92a8ec50c6794d1b1edd
Manuel Gutierrez <m.gutierrez@druo.com>
2022-06-10 10:02:33 -0500

    Add: Final Code.

--- sketch-01.js
+++ sketch-01.js
@@ -8,16 +8,41 @@ const settings = {
 };
 
 /*
-Idea: Create an abstraction of a trompet,
-representing Salsa Music, and my visit to Cali.
+Idea: Create an abstraction of a trumpet,
+representing Salsa Music.
+
+Plot figures in the canvas plain by using 
+basic concepts of trigonometry, applying transforms.
 */
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    // Draw the white canvas with the dimensions defined in settings.
-    context.fillStyle = 'red';
+
+    // Margin in inches
+    const margin = 1 / 4;
+
+    // Off-white background
+    context.fillStyle = 'hsl(0, 0%, 0%)';
     context.fillRect(0, 0, width, height);
-    context.fillStyle = 'white';
+
+    // Gradient foreground
+    const fill = context.createLinearGradient(0, 0, width, height);
+    fill.addColorStop(0, '#d7263d');
+    fill.addColorStop(1, '#2b2d42');
+
+    // Fill rectangle
+    context.fillStyle = fill;
+    context.fillRect(margin, margin, width - margin * 2, height - margin * 2);
+
+
+
+    // Draw the canvas.
+    // context.fillStyle = '#d7263d';
+    // context.fillRect(0, 0, width, height);
+
+    //Set the filling color for the objects.
+    context.fillStyle = '#370617';
+    context.strokeStyle = '#ffba08';
 
     /* 
     Setup the center coordinates of the canvas,
@@ -28,58 +53,76 @@ const sketch = () => {
 
     // Rectangles Width and Height.
     const w = width * 0.01;
-    const h = height * 0.1;
+    const h = height * 0.14;
 
     /* 
     Number of Figures divided in a ful circle (360deg) 
     So 12 iterations means that the a slice will be 
     drawn every 30 degrees.
     */
-    const iterations = 100;
+    const iterations = 50;
+
     //To use the context.rotation we need to covert this to RAD.  
     const slice = math.degToRad(360 / iterations);
 
-    // Radius based on the width of the canvas.
-    const radius = height * 0.5;
-    console.log(radius)
+    // Circle minimum Radius, for the trompet.
+    // TO DO: Play with the radius to create somenthing like a cone. 
+    const radius = width * .3;
 
-    let x, y;
+    // New Origins and Angles to transform the origin.
+    let x, y, angle;
 
-
-    for (let i = 0; i < iterations; i++) {
-
-
-
-      const angle = slice * i;
-
-      x = cx + radius * Math.sin(angle);
-      y = cy + radius * Math.cos(angle);
-
+    function randomRects(x, y, angle, w, h) {
       context.save();
-
-
+      //Move the origin to x and y.
       context.translate(x, y);
-      context.rotate(-angle);
-      context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
-
       context.beginPath();
-      context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
+      context.rotate(-angle);
+      context.scale(random.range(0.1, 1.9), random.range(0.1, 0.9));
+      context.rect(w * 0.5, random.range(0, h * 0.5), w, h);
       context.fill();
-
       context.restore();
+    };
 
+    function randomArc(cx, cy, angle) {
       context.save();
       context.translate(cx, cy);
       context.rotate(-angle);
-
-      context.lineWidth = random.range(5, 20);
-
+      context.lineWidth = random.range(5, 10);
       context.beginPath();
-      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
+      context.arc(0, 0, radius * random.range(0.2, 1.3), slice * random.range(1, -9), slice * random.range(1, 5));
       context.fill();
       context.stroke();
-
       context.restore();
+    }
+
+
+    function linesFromCenter(cx, cy) {
+      context.beginPath();
+      context.moveTo(cx, cy);
+      context.lineWidth = random.range(0.1, 3);
+      context.lineTo(x, y);
+      context.stroke();
+    }
+
+    for (let i = 0; i < iterations; i++) {
+
+      // Set the angle to the first point in the cirle.
+      angle = slice * i;
+
+      // Define position in the plane.
+      //Sin(0), Cos(0) = 0,1
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      /* 
+      First block: 
+      Draw some random rectangles around a circle, 
+      with diferent sizes. 
+      */
+      linesFromCenter(cx, cy);
+      randomArc(cx, cy, angle);
+      randomRects(x, y, angle, w, h)
     }
   };
 };
