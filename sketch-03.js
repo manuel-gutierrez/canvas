a6cc8ca20c90c616941b90ce55162b082d11ae46
Manuel Gutierrez <m.gutierrez@druo.com>
2022-12-11 21:34:56 -0500

    update: sketch versions

--- sketch-03.js
+++ sketch-03.js
@@ -1,121 +1,156 @@
-const canvasSketch = require('canvas-sketch');
-const random = require('canvas-sketch-util/random');
-const math = require('canvas-sketch-util/math');
-import { v4 as uuidv4 } from 'uuid';
+const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
+const math = require("canvas-sketch-util/math");
+// import { v4 as uuidv4 } from 'uuid';
+
 const settings = {
-  dimensions: [2048, 2048],
+  // Enable an animation loop
+
   animate: true,
+  // Set loop duration to 3
+  // duration: 0.2,
+  // Use a small size for better GIF file size
+  dimensions: [1020, 1080],
+  // Optionally specify a frame rate, defaults to 30
+  fps: 30,
 };
 
+// Globals
+const minDistanceBetweenCircles = 90;
+const circleStrokeColor = "#1d3557";
+const lineColor = "#e63946";
+const numberOfCirclesInScreen = 20;
+const maxV = 10.3;
+const minV = -2.3;
 
-const sketch = ({ context, width, height }) => {
-  // hold the points. 
-  const points = []
-  // Number of points
-  const numberOfPoints = 6;
-
-
-  for (let index = 0; index < numberOfPoints; index++) {
-    let v = { x: random.range(0, width), y: random.range(0, height), r: random.range(0, 20) }
-    points.push(new Point(v.x, v.y, v.r))
-  }
-  console.log(points)
-
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+canvasSketch(({ context, width, height, playhead }) => {
+  // Variables
+  const randomXRangeLimit = width * 0.9;
+  const randomYRangeLimit = height * 0.9;
+  // Create Array of Circles.
+  const circles = bulkCircleCreation(
+    numberOfCirclesInScreen,
+    randomXRangeLimit,
+    randomYRangeLimit
+  ); // create an array of points to draws
+  // Loop Starts
+  return ({ context, width, height, playhead }) => {
+    context.fillStyle = "#f1faee";
     context.fillRect(0, 0, width, height);
-    // Draw points per frame
-    points.forEach(point => {
-      point.updatePosition();
-      point.edgeContain(width, height);
-      point.collide(points, context);
-      point.lineBetweenToPoints(context, points)
-      point.drawPoint(context);
+    // Animate the cirles
+    circles.forEach((circle) => {
+      circle.update();
+      circle.wrap(width, height); // pacman mode
+      circle.bounce(width, height); // billard mode
+      circle.draw(context, circleStrokeColor);
+    });
+    drawLineBetweenCircles();
+
+    // eslint-disable-next-line require-jsdoc
+    function drawLineBetweenCircles() {
+      // Start with first Element.
+      for (let index = 0; index < circles.length; index++) {
+        const element = circles[index];
+        // Start with first Element.
+        for (let nextIndex = 1; nextIndex < circles.length; nextIndex++) {
+          const nextElement = circles[nextIndex];
+          // compare two distances
+          const d = element.pos.getDistance(nextElement.pos);
+          if (d < minDistanceBetweenCircles) {
+            // draw a line between the two elements.
+            element.drawLine(context, nextElement, d);
+          } else {
+            continue;
+          }
+        }
+      }
     }
-    );
   };
-};
-
-
-canvasSketch(sketch, settings);
-
+}, settings);
+/**
+ * Add two numbers.
+ * @param {x} num1 The first number.
+ * @param {y} num2 The second number.
+ * @return {Vector} The sum of the two numbers.
+ */
 class Vector {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
-  getDistance(v) {
-    const dx = this.x - v.x;
-    const dy = this.y - v.y;
+
+  getDistance(vect) {
+    const dx = this.x - vect.x;
+    const dy = this.y - vect.y;
     return Math.sqrt(dx * dx + dy * dy);
   }
 }
-class Point {
-  constructor(x, y, r) {
-    this.position = new Vector(x, y);
-    this.velocity = new Vector(random.range(-10, 10), random.range(-1, 1));
-    this.r = r;
-    this.id = uuidv4();
+
+class Circle {
+  constructor(x, y) {
+    // Position  and velocity
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(random.range(maxV, minV), random.range(maxV, minV));
+    // Radius
+    this.radius = random.range(3, 20);
   }
 
-  updatePosition() {
-    this.position.x += this.velocity.x;
-    this.position.y += this.velocity.y;
-  }
-
-  drawLine(context, target, lineWidht) {
-    context.lineWidth = lineWidht;
+  draw(context, color) {
+    context.save();
+    context.translate(this.pos.x, this.pos.y);
+    context.lineWidth = 4;
+    context.lineColor = lineColor;
     context.beginPath();
-    context.moveTo(this.position.x, this.position.y);
-    context.lineTo(target.x, target.y);
+    context.arc(0, 0, this.radius, 0, Math.PI * 2);
+    context.strokeStyle = circleStrokeColor;
     context.stroke();
     context.restore();
   }
-  drawPoint(context) {
+  drawLine(context, element, distance) {
     context.beginPath();
-    context.save();
-    context.translate(this.position.x, this.position.y);
-    context.arc(0, 0, this.r, 0, Math.PI * 2);
-    context.fillStyle = "darkgreen";
-    context.fill();
-    context.restore();
-  }
-  // Chage the velocity vector by x value
-  changeVelocity(value) {
-    const newVelocity = new Vector(this.velocity.x *= value, this.velocity.y *= value)
-    return newVelocity;
-  }
-  //bounce when it touches the edge.
-  edgeContain(width, height) {
-    if (this.position.x <= 0 || this.position.x >= width) this.velocity.x *= -1;
-    if (this.position.y <= 0 || this.position.y >= height) this.velocity.y *= -1;
+    context.lineWidth = math.mapRange(
+      distance,
+      0,
+      minDistanceBetweenCircles,
+      1,
+      3
+    );
+    context.moveTo(element.pos.x, element.pos.y);
+    context.lineTo(this.pos.x, this.pos.y);
+    context.strokeStyle = lineColor;
+    context.stroke();
   }
 
-  collide(points) {
-    // To calculate if the circles collide, the distance between centers > R+R == true 
-    // https://www.youtube.com/watch?v=LPzyNOHY3A4
-
-    // remove the instance of the actual point.
-    let data = points.filter(point => this.id !== point.id);
-
-    data.forEach(otherPoint => {
-      // Calculate the distance between two centres.
-      const distance = this.position.getDistance(otherPoint.position);
-      // Calculate the sum of the radius
-      const radius = otherPoint.r + this.r
-      // Check if the condition is met.
-      if (distance < radius) {
-        otherPoint.changeVelocity(-1);
-        this.changeVelocity(-1);
-      }
-    })
+  // Bounce with the limits of the screen.
+  bounce(width, height) {
+    if (this.pos.x <= 0 || this.pos.x >= width) this.vel.x *= -1;
+    if (this.pos.y <= 0 || this.pos.y >= height) this.vel.y *= -1;
   }
-  // Draw a line based on the distance between to points.
-  lineBetweenToPoints(context, points) {
-    let data = points.filter(point => this.id !== point.id);
-    data.forEach(point => {
-      const dist = this.position.getDistance(point.position);
-      this.drawLine(context, point.position, math.mapRange(dist, 0, 200, 12, 0));
-    })
+
+  // Bounce with the limits of the screen.
+  wrap(width, height) {
+    if (this.pos.x >= width) this.pos.x = 0;
+    if (this.pos.y >= height) this.pos.y = 0;
+  }
+
+  // Move a step the position of the circle.
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+
+  // logs position.
+  position() {
+    console.log("Pos x:" + this.x, "Pos y:" + this.y);
   }
 }
+
+const bulkCircleCreation = (numberOfCircles, rangeX, rangeY) => {
+  const circleArray = [];
+  for (let index = 0; index < numberOfCircles; index++) {
+    circleArray.push(
+      new Circle(random.range(0, rangeX), random.range(0, rangeY), 20)
+    );
+  }
+  return circleArray;
+};
---
+++ sketch-04.js
@@ -0,0 +1,89 @@
+/* 
+Desc: Unit 5 creating grids. This will create a grid and add some 
+randomness to the mix to create noise and movement felling.
+*/
+const canvasSketch = require('canvas-sketch');
+
+
+const settings = {
+  dimensions: [1000, 1000]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    // Context
+    context.fillStyle = 'red';
+    context.fillRect(0, 0, width, height);
+
+    // Grid Object init
+    let grid = {
+      width: width * 0.9,
+      height: height * 0.9,
+      cols: 4,
+      rows: 4,
+      cell: { width: 0, height: 0 },
+      cellsNumber: 0,
+    }
+
+    // Calculate margins.
+    grid.yMargin = (height - grid.height) * 0.5
+    grid.xMargin = (width - grid.width) * 0.5
+
+    // 
+    context.fillStyle = 'white';
+    context.fillRect(grid.xMargin, grid.yMargin, grid.width, grid.height);
+    console.log(grid);
+
+
+    // Calculate cells property. 
+    grid.cellsNumber = grid.cols * grid.rows;
+
+    grid.cell.width = grid.width / grid.cols;
+    grid.cell.height = grid.height / grid.rows;
+
+
+    for (let index = 0; index < grid.cellsNumber; index++) {
+
+      // Lopping col and row
+      const col = index % grid.cols // 0 to cols 
+      const row = Math.floor(index / grid.cols) // 0 to row , then  +1 
+
+
+      // x,y Position of the cell 
+      let x = col * grid.cell.width;
+      console.log("🚀 ~ file: sketch-04.js:54 ~ return ~ x", x)
+      let y = row * grid.cell.height;
+      ; // Horizontal line.
+
+      // Reduce the Area of the cell so they do not overlap.
+      const w = grid.cell.width * 0.9;
+      const h = grid.cell.height * 0.9;
+
+
+      //Draw line
+      context.save();
+      context.translate(x, y); // Translate to cell top left
+
+      context.translate(grid.xMargin, grid.yMargin) // 
+      context.translate(grid.cell.width * 0.5, grid.cell.height * 0.5) // translate to cell center
+      drawLine((w * -0.5), 0, (w * 0.5), 0, context); // Horizontal line.
+      drawLine(0, (h * -0.5), 0, (h * 0.5), context); // Horizontal line.
+      context.restore();
+
+    }
+
+
+  }
+
+}
+function drawLine(cx, cy, x, y, context) {
+  context.beginPath();
+  context.moveTo(cx, cy);
+  context.lineWidth = 5;
+  context.lineTo(x, y);
+  context.stroke();
+}
+
+canvasSketch(sketch, settings);
+
+
